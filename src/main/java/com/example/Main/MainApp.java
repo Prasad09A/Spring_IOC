f51f940cda02f7f81e959fb663aa36854f23f77d
Prasad A <prasadamilkanthwar2@gmail.com>
2025-12-06 00:30:03 +0530

    Final commit

---
+++ src/main/java/com/example/Main/MainApp.java
@@ -0,0 +1,21 @@
+package com.example.Main;
+
+import com.example.ioc.Student;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+    public static void main(String[] args) {
+        ApplicationContext context= new ClassPathXmlApplicationContext("ApplicationContext.xml");
+
+        Student std1= (Student)context.getBean("StdId1");
+        std1.display();
+
+        System.out.println("------------------------------------");
+
+        Student std2= (Student) context.getBean("StdId2");
+        std2.display();
+
+
+    }
+}
